0ff3fd1d0ac8a2d800c9f57dfa098cfc2878f581
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-15 20:22:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on('message', function (message) {
 
 
 bot.on('message', function (message) {
-  if(message.content.startsWith('h$annonce')&&message.author.id='300896265078571009, 283144121718276096, id_du_mec') {
+  if(message.content.startsWith('h$annonce')&&message.author.id='300896265078571009, 283144121718276096' {
       serv = bot.guilds.array()
       text = message.content.substr(9)
       for(i=0;i<serv.length;i++){
