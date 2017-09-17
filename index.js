6c7c534e720cf489b51718516a013a74d6883957
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-17 17:28:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,22 @@ bot.on('message', function (message) {
 
 //                             COMMAND PRIVE OR SETTHINGS ( COMMAND )
 
+
+if(message.content.startsWith("h$annonce")) {
+        if(message.author.id === "300896265078571009" || message.author.id === "283144121718276096" || message.author.id === "307588259968581634") {
+            var messageactu = message.content.substr(7);
+        
+            message.channel.send("Hey @everyone, " + messageactu);
+            message.delete(messageactu);
+        } else {
+            message.reply("**Erreur**\n:x:Vous n'avez pas les permissions nécessaires");
+        }
+
+        console.log("Commande exécuté : news");
+    }
+
 bot.on('message', function (message) { 
-if (message.content.startsWith('h$annonce')&&( message.author.id =='300896265078571009'|| message.author.id =='283144121718276096'|| message.author.id =='307588259968581634') )
+if (message.content.startsWith('')&&( message.author.id =='300896265078571009'|| message.author.id =='283144121718276096'|| message.author.id =='307588259968581634') )
       serv = bot.guilds.array()
       text = message.content.substr(9)
       for(i=0;i<serv.length;i++){
