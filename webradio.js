0d1fb93e260178d55e11e18eb721755dd5148b61
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-18 20:49:26 +0200

    Add files via upload

---
+++ webradio.js
@@ -0,0 +1,27 @@
+function webradio(message, prefix, client, connection, broadcast) {
+    if(message.content.startsWith(prefix + "webradioplay")) {
+        var fluxwebradio = message.content.substr(15);
+        message.member.voiceChannel.join()
+        .then(connection => {
+            require("http").get(fluxwebradio, (res) => {
+                connection.playStream(res);
+                message.channel.send("Lecture du flux webradio suivante :\n
+				
+				\nBonne écoute !");
+            });
+        });
+        client.user.setGame(fluxwebradio).then(ok => {
+            console.log("Changement de jeu effectué !");
+        });
+
+        console.log("Commande exécuté : webradioplay");
+    }
+
+    if(message.content.startsWith(prefix + "webradiostop")) {
+        message.member.voiceChannel.leave();
+        message.channel.send("Arrêt de la webradio. Merci de nous avoir écouté !");
+        console.log("Commande exécuté : webradiostop");
+    }
+}
+
+module.exports = webradio;
