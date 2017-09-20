f024e364f9d997acb040afe682f36486ae94d6d8
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-20 12:38:40 +0200

    Update webradio.js

--- webradio.js
+++ webradio.js
@@ -1,25 +1,5 @@
-function webradio(message, client, connection, broadcast) {
-    if(message.content.startsWith("h$webradioplay")) {
-        var fluxwebradio = message.content.substr(15);
-        message.member.voiceChannel.join()
-        .then(connection => {
-            require("http").get(fluxwebradio, (res) => {
-                connection.playStream(res);
-                message.channel.send("Lecture du flux webradio suivante :\n\nBonne écoute !");
-            });
-        });
-        client.user.setGame(fluxwebradio).then(ok => {
-            console.log("Changement de jeu effectué !");
-        });
-
-        console.log("Commande exécuté : webradioplay");
-    }
-
-    if(message.content.startsWith("h$webradiostop")) {
-        message.member.voiceChannel.leave();
-        message.channel.send("Arrêt de la webradio. Merci de nous avoir écouté !");
-        console.log("Commande exécuté : webradiostop");
-    }
-}
-
-module.exports = webradio;
+-function webradio(message, client, connection, broadcast) {
+ +function webradio(message, bot, connection, broadcast) {
+      if(message.content.startsWith("h$webradioplay")) {
+          var fluxwebradio = message.content.substr(15);
+          message.member.voiceChannel.join()
