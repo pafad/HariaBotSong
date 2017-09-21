bf30eec7c9e4c6f71e502b10b8ca8ec5104c001b
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-21 17:05:51 +0200

    Add files via upload

---
+++ modules/owner/botmessage.js
@@ -0,0 +1,24 @@
+function botmessage(message, bot) {
+	if(message.content === "h$botmessage") {
+		if(message.author.id === "300896265078571009") {
+			message.author.send("`Je suis connecté sur ces serveurs :\n`")
+			.then(m => {
+				client.guilds.map(g => {
+					message.author.send("\n```" + g.name + "```"); 
+				});
+			})
+			.catch(err => {
+				console.log(err);
+			});
+		} else {
+			message.channel.send({
+				embed: {
+					color: 0xFF0000,
+					description: "Erreur\n:x:Vous avez pas les permissions nécessaires ou il y a que le développeur qui à accès à la commande. Pour voir ou le bot est connecté, demander sur le serveur officiel. Merci."
+				}
+			});
+		}
+	}
+}
+
+module.exports = botmessage;
