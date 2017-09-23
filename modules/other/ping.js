80ad61566f957bc04a7cca3a3b7aba3f25da32d6
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-23 10:13:03 +0200

    Add files via upload

--- modules/other/ping.js
+++ modules/other/ping.js
@@ -1,9 +1,19 @@
 function ping(message, bot) {
 	if(message.content === "h$ping") {
 		let start = Date.now();
-		message.channel.send("Ping en cours ...").then(m => {
+		message.channel.send({
+			embed: {
+				color: 0xFF0000,
+				title: "Ping en cours ..."
+			}
+		}).then(m => {
 			let end = Date.now();
-			m.edit(`Pong : **${Math.round(end - start)}** ms`);
+			m.edit({
+				embed: {
+					color: 0xFF0000,
+					title: `:ping_pong: Pong : **${Math.round(end - start)}** ms`
+				}
+			});
 		});
 	}
 }
