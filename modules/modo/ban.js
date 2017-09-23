4ef59dac9845cfa6d75b909dc868ca4237990270
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-23 12:58:41 +0200

    Update ban.js

--- modules/modo/ban.js
+++ modules/modo/ban.js
@@ -6,10 +6,10 @@ function ban(message, bot) {
 		// Ban
 		member.ban().then((member) => {
 			// Successmessage
-			message.channel.send(":wave: " + member.displayName + " a été ban du serveur :ballot_box_with_check:").catch(console.erreur);
+			message.channel.send({embed: { color: 0xFF0000, description:":wave: " + member.displayName + " a été ban du serveur :ballot_box_with_check:"}}).catch(console.erreur);
 		}).catch(() => {
 			// Failmessage
-			message.channel.send(":x: **Erreur :** Vous n'avez pas les permissions !").catch(console.erreur);
+			message.channel.send({embed: { color: 0xFF0000, description:":x: **Erreur :** Vous n'avez pas les permissions !"}}).catch(console.erreur);
 		});
 	}
 }
