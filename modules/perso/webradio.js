4daaff05e7778f1fc71ff58ec3dd132f9c847281
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-09-23 11:18:45 +0200

    Update webradio.js

--- modules/perso/webradio.js
+++ modules/perso/webradio.js
@@ -5,7 +5,7 @@ function webradio(message, bot, connection, broadcast) {
         .then(connection => {
             require("http").get(fluxwebradio, (res) => {
                 connection.playStream(res);
-                message.channel.send("Lecture du flux webradio suivante :\n```" + fluxwebradio + "```\nBonne écoute !");
+                message.channel.send({embed: { color: 0xFF0000, description:("Lecture du flux webradio suivante :\n```" + fluxwebradio + "```\nBonne écoute !"}});
             });
         });
         bot.user.setGame(fluxwebradio).then(ok => {
@@ -17,7 +17,7 @@ function webradio(message, bot, connection, broadcast) {
 
     if(message.content.startsWith("h$webradiostop")) {
         message.member.voiceChannel.leave();
-        message.channel.send("Arrêt de la webradio. Merci de nous avoir écouté !");
+        message.channel.send({embed: { color: 0xFF0000, description:"Arrêt de la webradio. Merci de nous avoir écouté !"}});
         console.log("Commande exécuté : webradiostop");
     }
 }
