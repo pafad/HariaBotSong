dfa11f0dbb2c0838488991f75583b8400e01f946
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-01 17:37:02 +0200

    Create say.js

---
+++ modules/owner/say.js
@@ -0,0 +1,7 @@
+function annonce(message, bot) {
+	if(message.content.startsWith("h$say")) {
+               var messageactu = message.content.substr(9);
+			message.channel.send.messageactu;
+			message.delete(messageactu)}};
+
+module.exports = say;
