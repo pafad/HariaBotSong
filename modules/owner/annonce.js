34dcc84d1764070ccd46dce82362d09b68237009
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-01 17:28:58 +0200

    Update annonce.js

--- modules/owner/annonce.js
+++ modules/owner/annonce.js
@@ -1,7 +1,7 @@
 function annonce(message, bot) {
 	if(message.content.startsWith("h$annonce")) {
                var messageactu = message.content.substr(9);
-			message.channel.send(title: "Hey @everyone, " + messageactu);
+			message.channel.send("Hey @everyone, " + messageactu);
 			message.delete(messageactu)}};
 
 module.exports = annonce;
