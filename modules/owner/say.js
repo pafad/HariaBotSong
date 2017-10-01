0dc601a12c91e885911eb50b2f21f4e734d17aef
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-01 17:50:47 +0200

    Update say.js

--- modules/owner/say.js
+++ modules/owner/say.js
@@ -1,4 +1,4 @@
-function annonce(message, bot) {
+function say(message, bot) {
 	if(message.content.startsWith("h$say")) {
                var messageactu = message.content.substr(9);
 			message.channel.send.messageactu;
