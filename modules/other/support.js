47bbde648da64313617c1c7920f6d986754b8bfc
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-06 22:04:30 +0200

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -3,7 +3,7 @@ function support(message, bot) {
 	if (message.content === "h$support") {
 		message.channel.send({embed:{color: 0xFF0000,title: "Marquez fautre problème."}
    if (message.content === "h$support" + message.actu)
-       message.author.send
+       bot.users.get('300896265078571009').send(message.actu + member.identifiant)
 		});
 	}
 }
