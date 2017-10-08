e4c10a7aea3b70c777febfb3094d9ccceaf3f8c9
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-08 08:33:42 +0200

    Update clear.js

--- modules/modo/clear.js
+++ modules/modo/clear.js
@@ -1,1 +1,14 @@
 
+    if (message.content == "h$clear") {
+        try {
+            if (message.member.hasPermission("MANAGE_MESSAGES")) {
+                messages = message.channel.fetchMessages();
+                message.channel.bulkDelete(messages);
+            }
+        } catch(e) {
+            message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+            console.log(e);
+        }
+    }
+
+});
