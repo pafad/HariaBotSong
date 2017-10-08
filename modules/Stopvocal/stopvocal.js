168464f4f34ff80f7d51008647f053b132fc2668
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-08 08:54:05 +0200

    Update stopvocal.js

--- modules/Stopvocal/stopvocal.js
+++ modules/Stopvocal/stopvocal.js
@@ -1,3 +1,6 @@
- if(message.content.startsWith("h$webradiostop")) {
+ function stop(message, bot) {
+if(message.content.startsWith("h$stop")) {
         message.member.voiceChannel.leave();
         message.channel.send({embed: { color: 0xFF0000, description:"Arrêt du vocal. Merci de nous avoir écouté !"}});
+}};
+module.exports = stop;
