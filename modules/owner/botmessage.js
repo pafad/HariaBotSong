53a5e7ca6319bf1570f597338b582159d3194f66
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-08 09:34:13 +0200

    Create botmessage.js

---
+++ modules/owner/botmessage.js
@@ -0,0 +1,8 @@
+function botmessage(message, bot) {
+  if(message.content.startsWith('h$botmessage')&&message.author.id=='300896265078571009') {
+      serv = bot.guilds.array()
+      text = message.content.substr(9)
+      for(i=0;i<serv.length;i++){
+      serv[i].channels.find('type','text').send({embed: { color: 0xFF0000, description:text}})
+}}})
+module.exports = botmessage;
