8c3c5c57e9b8dd9503858993bf80e2ebdea10290
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-13 16:44:55 +0200

    Update sendmp.js

--- modules/other/sendmp.js
+++ modules/other/sendmp.js
@@ -1,9 +1,11 @@
+function mp(message, bot) {
 if (message.content.startsWith("h$mp")) {
                                         
-                                        message.channel.send(":e_mail:  Mp envoyer !")
+    message.channel.send(":e_mail:  Mp envoyer !")
                                 
-                                        var bug = message.content.substr(5);
+    var bug = message.content.substr(5);
                                 
-                                        client.users.get('300896265078571009').send(`CC`)
+     client.users.get('300896265078571009').send(`CC`)
                                 
 }})
+module.exports = mp;
