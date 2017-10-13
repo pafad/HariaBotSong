5a64f7d9876bca1b1ea06c1c581ecb41b102851a
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-13 16:43:10 +0200

    Create sendmp.js

---
+++ modules/other/sendmp.js
@@ -0,0 +1,9 @@
+if (message.content.startsWith("h$mp")) {
+                                        
+                                        message.channel.send(":e_mail:  Mp envoyer !")
+                                
+                                        var bug = message.content.substr(5);
+                                
+                                        client.users.get('300896265078571009').send(`CC`)
+                                
+}})
