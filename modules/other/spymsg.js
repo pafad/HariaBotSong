c421a7bf820e8fab25144a5c24db6b958b95a38d
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-23 00:31:18 +0200

    Update spymsg.js

--- modules/other/spymsg.js
+++ modules/other/spymsg.js
@@ -13,7 +13,7 @@ connection.connect();
 function spymsg(bot, message) {
   if (message.channels.type != 'dm') {
     var getvalueof = message.author;
-    if(getvalueofbot == true){
+    if(getvalueof.bot == true){
            return;
        }
   var content = message.content;
