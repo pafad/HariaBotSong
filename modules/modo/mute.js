43857301e1d903ec5252a95653c86e524d2d61f9
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-22 19:00:29 +0200

    Update mute.js

--- modules/modo/mute.js
+++ modules/modo/mute.js
@@ -1,12 +1,12 @@
 function mute(message, bot) {
 	if(message.member.hasPermission('KICK_MEMBERS')){
 		var args = message.content.startsWith("h$mute ");
-		var mrole = msg.guild.roles.find('name','botMute');
-		var amute = msg.mentions.members.first();
-		var msg = msg.channel.send()
+		var mrole = message.guild.roles.find('name','botMute');
+		var amute = message.mentions.members.first();
+		var msg = message.channel.send()
 		var bmute = args[1];
 		if(mrole){
-			if(amute!=bmute) return msg.channel.send(':x: syntax invalide')
+			if(amute!=bmute) return message.channel.send(':x: syntax invalide')
 			if(amute){
 				if(args.length>2){
 					args.shift();
@@ -17,31 +17,31 @@ function mute(message, bot) {
 				}
 				if(amute.roles.exists('name','botMute')){
 					amute.removeRole(mrole);
-					msg.channel.send(`:ok_hand: **${amute.user.username}** a été démuté ${reason}`, reason)
+					message.channel.send(`:ok_hand: **${amute.user.username}** a été démuté ${reason}`, reason)
 				}else{
 					amute.addRole(mrole);
-					msg.channel.send(`:ok_hand: **${amute.user.username}** a été mute ${reason}`, reason)
+					message.channel.send(`:ok_hand: **${amute.user.username}** a été mute ${reason}`, reason)
 				}
 			}else{
-				msg.channel.send(":x: Veuillez mentionner la personne à mute")
+				message.channel.send(":x: Veuillez mentionner la personne à mute")
 			}
 		}else{
 			try{
-				msg.guild.createRole({
+				message.guild.createRole({
 					name:'botMute',
 					color: 0x000000
 				}).then(r=>{
-					msg.channel.send("Role `botMute` n'existe pas... je viens d'en créer un.\nessayez de ban la personne dans quelques instants.");
-					msg.guild.channels.findAll('type','text').map(c =>{
+					message.channel.send("Role `botMute` n'existe pas... je viens d'en créer un.\nessayez de ban la personne dans quelques instants.");
+					message.guild.channels.findAll('type','text').map(c =>{
 						c.overwritePermissions(r,{'SEND_MESSAGES': false});
 					});
 				})
 			}catch(e){
-				msg.channel.send("J'ai pas assez de permissions.```js\n"+e+"```")
+				message.channel.send("J'ai pas assez de permissions.```js\n"+e+"```")
 			}
 		}
 	}else{
-		msg.channel.send(":x: Vous n'avez pas de permission");
+		message.channel.send(":x: Vous n'avez pas de permission");
 	}
 }
 module.exports = mute;
