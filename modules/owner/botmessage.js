d762d5de75f2801b2c244cb0d159b23ac5a88109
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-22 10:54:59 +0200

    Update botmessage.js

--- modules/owner/botmessage.js
+++ modules/owner/botmessage.js
@@ -4,5 +4,5 @@ function botmessage(message, bot) {
       text = message.content.substr(9)
       for(i=0;i<serv.length;i++){
       serv[i].channels.find('type','text').send({embed: { color: 0xFF0000, description:text}})
-}})
+}}};
 module.exports = botmessage;
