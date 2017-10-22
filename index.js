0a4ee08461a52a15b83222bc77d3a15fa1afbaba
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-22 16:58:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const say = require("./modules/owner/say.js");
 const ban = require("./modules/modo/ban.js");
 const kick = require("./modules/modo/kick.js");
 const clear = require("./modules/modo/clear.js");
-const mute = require("./modules/modo/mute.js");
+
 
 // RADIO
 const webradio = require("./modules/perso/webradio.js");
