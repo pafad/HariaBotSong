0130af0f5adc3e65fa1f9161889f65f1229ea4dd
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-23 16:50:50 +0200

    Update mute.js

--- modules/modo/mute.js
+++ modules/modo/mute.js
@@ -11,7 +11,7 @@ function mute(message, bot) {
 if (message.guild.member(target).roles.has(muteRole.id)) {
     message.guild.member(target).removeRole(muteRole).then(() => {
       message.channel.send("Muted " + target + " Reason: " + reason);
-    }};
+    )}};
   } else {
     message.guild.member(target).addRole(muteRole).then(() => {
       message.channel.send("Muted. " + target + " Reason: " + reason);
