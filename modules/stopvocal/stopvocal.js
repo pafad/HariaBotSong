4aad0df21ba722f3d51d1196055eed23b2887a70
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-26 21:23:02 +0200

    Delete stopvocal.js

--- modules/stopvocal/stopvocal.js
+++
@@ -1,6 +0,0 @@
- function stop(message, bot) {
-if(message.content.startsWith("h$stop")) {
-        message.member.voiceChannel.leave();
-        message.channel.send({embed: { color: 0xFF0000, description:"Arrêt du vocal. Merci de nous avoir écouté !"}});
-}};
-module.exports = stop;
