c23d1419fb1ba54dc015c3dbe2650c06ff029ed4
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-26 11:43:22 +0200

    Update botmessage.js

--- modules/owner/botmessage.js
+++ modules/owner/botmessage.js
@@ -1,7 +1,7 @@
 function botmessage(message, bot) {
   if(message.content.startsWith('h$botmessage')&&message.author.id=='300896265078571009') {
       serv = bot.guilds.array()
-      text = message.content.substr(9)
+      text = message.content.substr(12)
       for(i=0;i<serv.length;i++){
       serv[i].channels.find('type','text').send({embed: { color: 0xFF0000, description:text}})
 }}};
