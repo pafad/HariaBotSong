e6033ad664426ca35bf351a633a6151bb503ec92
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-26 22:22:47 +0200

    Update helpaudio.js

--- modules/audio/helpaudio.js
+++ modules/audio/helpaudio.js
@@ -1,7 +1,8 @@
-function help(message, bot) {
+function helpaudio(message, bot) {
 	if (message.content === "h$helpaudio") {
 		message.channel.send({embed:{ color: 0xFF0000, description:":envelope_with_arrow: Help envoyer en mp !"}})
-	     message.author.send({embed:{footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},color: 0xFF0000, description: "[**Commandes:**]()\n\n" +
+	     message.author.send({embed:{footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},color: 0xFF0000, 
+					 description: 
 				"**[Commandes audio :]**\n\n" +
 				":white_check_mark:h$add : <recherche> ou h$add <URL vidéo> : Pour ajouter une musique recherché ou l'URL de la vidéo et met le bot dans un canal vocal.\n" +
 				":white_check_mark:h$playyt : Pour mettre la musique en lecture.\n" +
@@ -14,11 +15,12 @@ function help(message, bot) {
 				":white_check_mark:h$addlist : Pour ajouter une playlist YouTube.\n" +
 				":white_check_mark:h$queue : Pour afficher la/les musique(s) qui est/sont dans la file d'attente." 
 			}
+		});
+	}
 
-	
 	if (message.content === "h$helpaudioici") {
-	message.channel.send({embed:{footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},color: 0xFF0000,
-		   description: "[**Commandes:**]()\n\n" +
+		message.channel.send({embed:{footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},color: 0xFF0000,
+				description: 
 				"**[Commandes audio :]**\n\n" +
 				":white_check_mark:h$add : <recherche> ou h$add <URL vidéo> : Pour ajouter une musique recherché ou l'URL de la vidéo et met le bot dans un canal vocal.\n" +
 				":white_check_mark:h$playyt : Pour mettre la musique en lecture.\n" +
@@ -31,3 +33,7 @@ function help(message, bot) {
 				":white_check_mark:h$addlist : Pour ajouter une playlist YouTube.\n" +
 				":white_check_mark:h$queue : Pour afficher la/les musique(s) qui est/sont dans la file d'attente." 
 			}
+		});
+	}
+}
+module.exports = helpaudio;
