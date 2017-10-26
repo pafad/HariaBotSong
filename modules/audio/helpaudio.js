eabbbdfcc664d14bdab9130e0a8b44efeb333198
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-10-26 21:36:37 +0200

    Update helpaudio.js

--- modules/audio/helpaudio.js
+++ modules/audio/helpaudio.js
@@ -1,6 +1,6 @@
 function help(message, bot) {
 	if (message.content === "h$helpaudio") {
-		message.channel.send({embed:{ color: 0xFF0000, description:":envelope_with_arrow: Help envoyer en mp !"}})
+		message.channel.send({embed:{ color: 0xFF0000, description:":envelope_with_arrow: Helpaudio envoyer en mp !"}})
 	     message.author.send({embed:
 				  {footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},
 				  color: 0xFF0000, description: "**[Commandes audio :]**\n\n" +
@@ -13,4 +13,6 @@ function help(message, bot) {
 				":white_check_mark:h$volume- : Pour diminuer le volume de la musique en cours de lecture.\n" +
 				":white_check_mark:h$time : Affiche le temps écoulé de la musique en cours de lecture.\n" +
 				":white_check_mark:h$addlist : Pour ajouter une playlist YouTube.\n" +
-				":white_check_mark:h$queue : Pour afficher la/les musique(s) qui est/sont dans la file d'attente." +})
+				":white_check_mark:h$queue : Pour afficher la/les musique(s) qui est/sont dans la file d'attente." +
+}}	}}			  
+  )}				
