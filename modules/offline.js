9a1b5f4b141013b0eeb304bc652c9786b3618952
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-11-18 12:16:12 +0100

    Update offline.js

--- modules/offline.js
+++ modules/offline.js
@@ -1,16 +1,12 @@
 function offline(message, bot) {
-if (message.content == "h$offline")) {
+if (message.content == "h$offline") {
         if(message.author.id == "300896265078571009") {
             message.channel.send("Arrêt en cours ...");
-
             console.log("Arrêt en cours ...");
             console.log("/ Je suis désormais hors ligne (offline) /");
-
             client.destroy();
-
             process.exit();
         } else {
-            message.channel.send(":x: **Erreur** :x: ! Tu n'es pas l'owner !");
-        }
-    }
+            message.channel.send(":x: **Erreur** :x: ! Tu n'es pas l'owner !");}
+    }}};
     module.exports = offline;
