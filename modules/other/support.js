441dfcac9989710ba6460b46fd6d6c32f2b56655
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-22 19:05:33 +0100

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -3,13 +3,10 @@ function support(message, bot) {
 
         message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
        var Rapport = message.content.substr(10);
-       bot.users.get('300896265078571009','306119836503900161').send( `Rapport: ${Rapport} \nPseudo de la personne : @${message.author.username}\nSur le server : ${message.guild.name}\nDans le salon  #${message.channel.name}`)
+              //support du bot
+       bot.users.get('306119836503900161').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}\nDiscrimiminateur: #${message.author.discriminator}`)
+       bot.users.get('300896265078571009').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}\nDiscrimiminateur: #${message.author.discriminator}`)
 }};
 
-    if (message.content.startsWith("h$support")) {
-
-        message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
-       var Rapport = message.content.substr(10);
-       bot.users.get('306119836503900161').send( `Rapport: ${Rapport} \nPseudo de la personne : @${message.author.username}\nSur le server : ${message.guild.name}\nDans le salon  #${message.channel.name}`)
-}};
+ 
 module.exports = support;
