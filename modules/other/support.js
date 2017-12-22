f151e0796b1c2be8d4d27d10e616bcc86de2ac7d
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-22 19:09:57 +0100

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -4,8 +4,9 @@ function support(message, bot) {
         message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
        var Rapport = message.content.substr(10);
               //support du bot
-       bot.users.get('306119836503900161').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}\nDiscrimiminateur: #${message.author.discriminator}`)
-       bot.users.get('300896265078571009').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}\nDiscrimiminateur: #${message.author.discriminator}`)
+       bot.users.get('306119836503900161').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}`)
+       bot.users.get('300896265078571009').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}`)
+       bot.users.get('130979396134633472').send(`Rapport: ${Rapport} \nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}\nId de l'utilisateur: ${message.author.id}`)
 }};
 
  
