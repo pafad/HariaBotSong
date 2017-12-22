00a291b5adfb177c7043a0afe787981613e691b0
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-22 18:51:54 +0100

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -3,7 +3,14 @@ function support(message, bot) {
 
         message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
        var Rapport = message.content.substr(10);
-       bot.users.get('300896265078571009','306119836503900161').send({embed:{footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},color: 0xFF0000, description: 
-`Rapport: ${Rapport} \nPseudo de la personne : @${message.author.username}\nSur le server : ${message.guild.name}\nDans le salon  #${message.channel.name}}`)
+       bot.users.get('300896265078571009','306119836503900161').send( `Rapport: ${Rapport} \nPseudo de la personne : @${message.author.username}\nSur le server : ${message.guild.name}\nDans le salon  #${message.channel.name}}`)
+}};
+
+function support(message, bot) {
+    if (message.content.startsWith("h$support")) {
+
+        message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
+       var Rapport = message.content.substr(10);
+       bot.users.get('306119836503900161').send( `Rapport: ${Rapport} \nPseudo de la personne : @${message.author.username}\nSur le server : ${message.guild.name}\nDans le salon  #${message.channel.name}}`)
 }};
 module.exports = support;
