40025165d2112bbcf2eb422916706586abb4640b
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-22 19:16:08 +0100

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -1,7 +1,7 @@
 function support(message, bot) {
     if (message.content.startsWith("h$support")) {
 
-        message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
+        message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer à l'équipe de dev de HariaBotSong™!"}})
        var Rapport = message.content.substr(10);
               //support du bot
        bot.users.get('306119836503900161').send(`Rapport: ${Rapport} \nId de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
