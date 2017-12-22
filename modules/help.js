70191630135ac8a30c3d7d0ec4162d27ff5e6cc5
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-22 18:28:14 +0100

    Update help.js

--- modules/help.js
+++ modules/help.js
@@ -1,4 +1,4 @@
-function help(message, bot) {
+function help (message, bot) {
 	if (message.content === "h$help") {
 		message.channel.send({embed:{ color: 0xFF0000, description:":envelope_with_arrow: Help envoyer en mp !"}})
 	     message.author.send({embed:{footer:{text: "By Hariamane",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/853decbb9091b9a045c198c68e4866eb.png?size=128"},color: 0xFF0000, 
