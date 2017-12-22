0675f873870b4b9faece8d3a9b452fe460c7060b
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-22 18:57:56 +0100

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -6,7 +6,6 @@ function support(message, bot) {
        bot.users.get('300896265078571009','306119836503900161').send( `Rapport: ${Rapport} \nPseudo de la personne : @${message.author.username}\nSur le server : ${message.guild.name}\nDans le salon  #${message.channel.name}`)
 }};
 
-function support(message, bot) {
     if (message.content.startsWith("h$support")) {
 
         message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Rapport envoyer a Hariamane!"}})
