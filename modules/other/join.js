414d0b4f23ec9021b96f5f80c4ec258a5b3f14de
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-23 20:07:05 +0100

    Update join.js

--- modules/other/join.js
+++ modules/other/join.js
@@ -1,12 +1,6 @@
 function join(message, bot) {
 	if (message.content === "h$join") {
-		message.channel.send({
-			embed: {
-				color: 0xFF0000,
-				title: "https://discordapp.com/oauth2/authorize?client_id=329551988222066689&scope=bot"
-			}
-		});
+		message.channel.send("https://discordapp.com/oauth2/authorize?client_id=329551988222066689&scope=bot");
 	}
 }
-
 module.exports = join;
