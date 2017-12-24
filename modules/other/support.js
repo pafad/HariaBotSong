4c21deaeb80807a9e2c40b81adfed2640239da7e
Hariamane <31965369+Hariamane@users.noreply.github.com>
2017-12-24 14:12:24 +0100

    Update support.js

--- modules/other/support.js
+++ modules/other/support.js
@@ -7,7 +7,6 @@ function support(message, bot) {
        bot.users.get('306119836503900161').send(`Rapport: ${Rapport} \nId de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
        bot.users.get('300896265078571009').send(`Rapport: ${Rapport} \nId de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
        bot.users.get('130979396134633472').send(`Rapport: ${Rapport} \nId de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
+       bot.users.get('283144121718276096').send(`Rapport: ${Rapport} \nId de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
 }};
-
- 
 module.exports = support;
