a358cb6b59af3171bf678253577ac9125b804fca
Hariamane <31965369+Hariamane@users.noreply.github.com>
2018-01-28 20:26:33 +0100

    Update afk.js

--- modules/other/afk.js
+++ modules/other/afk.js
@@ -1,50 +1,44 @@
+function afk(message, bot){
 const fs = require("fs")
 var msg = message;
 var prefixe = "h$";
+var raison = message.content.substr(5);
 
 let afk = JSON.parse(fs.readFileSync("afk.json" , "utf8"));
-if message.content.startsWith(prefixe + "renafk")){
-  if(afk[msg.author.id]) {
-    delete afk[msg.author.id];
-if(msg.member.nickname === null) {
-  msg.channel.send({embed: { color: 0xFF0000, description:"J'ai enlèver ton AFK, car tes de retour ;)"}});
-}else{
-      msg.channel.send({embed: { color: 0xFF0000, description:"J'ai enlèver ton AFK, car tes de retour ;)"}});
-}
-fs writeFile("afks.json", JSON.stringify(afk), (err) => { if (err)console.error(err);));
-}else{
-message.channel.send({embed: { color: 0xFF0000, description:":x:**Erreur !** tu est déjà AFK"}});
-}
-}
 
-
-if (message.content.startswith(h$ + "afk")||message.content === prefix + "afk") {
-  if (afk[message.author.id]) {
+if (message.content === prefixe + "afk" + raison){
+  if (afk[message.author.id].reason) {
   return message.channel.send({embed: { color: 0xFF0000, description:":x:**Erreur !** tu est déjà AFK -_-"}});
 }else{
- let args1 = message.content.split(" ").slice(1);
-  if (args1.length === 0) {
-   afk[message.author.id] = {"reason" : true};
-   message.delete();
-    message.channel.send({embed: { color: 0xFF0000, description:'Tu es désormé **AFK**, met $(prefixe)remafk**pour enlèvé ton afk'}}).then(x => DeleteQueue.add(x, 10000));
+  if (raison.length === 0) {
+   afk[message.author.id] = {"reason" : message.content.substr(5)};
   }else{
-    afk[message.author.id] = {"reason" : args1.join( " ")};
-   message.delete();
-    message.channel.send({embed: { color: 0xFF0000, description:'Tu es désormé **AFK**, met $(prefixe)remafk**pour enlèvé ton afk'}}).then(x => DeleteQueue.add(x, 10000));
+    message.channel.send({embed: { color: 0xFF0000, description:`Tu es désormé **AFK** pour raison ${raison}`}}).then(x => DeleteQueue.add(x, 10000));
+    afk[message.author.id] = {"reason": message.content.substr(5)};
   }
-  fs.writeFile("afk.json", JSON.stringify(afk), (err) =>( if (err) 
-  console.error(err);)};
-}
+  fs.writeFile("afk.json", JSON.stringify(afk), (err) =>{ if (err)
+  console.error(err)});
 }
 
- var mentionned = message.mentions.users.first();
-if(msg.mentions.users.size > 0 {
-  if(afk[msg.mentions.users.first().id]) {
-  if(afk[msg.mentions.users.first().id].reason === true) {
-message.channel.send({embed: { color: 0xFF0000, description:'@$(mentionned.username) is **AFK**: pas de raison'}}) 
-  }else{
-    message.channel.send({embed: { color: 0xFF0000, description:'@$(mentionned.username) is **AFK**: $(afk[msg.mentions.users.first().id].reason'}}) 
+}
+if(message.content === prefixe + "reafk"){
+  if(afk[msg.author.id]) {
+    delete afk[msg.author.id].reason;
+  msg.channel.send({embed: { color: 0xFF0000, description:"J'ai enlèver ton AFK, car tes de retour ;)"}});
+fs.writeFile("afk.json", JSON.stringify(afk), (err) => { if (err)console.error(err)});
   }
-  
-  
-  
+}
+
+
+ var mentionned = message.mentions.users.first();
+if(msg.mentions.users.size > 0 ){
+  if(afk[msg.mentions.users.first().id]) {
+  if(afk[msg.mentions.users.first().id].reason) {
+message.channel.send({embed: { color: 0xFF0000, description:`${mentionned.username} is **AFK**: ${afk[msg.mentions.users.first().id].reason}`}}) 
+  }else{
+    return;
+      }
+    }
+  }
+}
+module.exports = afk;
