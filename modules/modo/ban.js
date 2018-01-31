52adc9176d174ec174c8650578c7e04ed5925241
Hariamane <31965369+Hariamane@users.noreply.github.com>
2018-01-31 17:43:55 +0100

    Add files via upload

--- modules/modo/ban.js
+++ modules/modo/ban.js
@@ -1,16 +1,31 @@
 function ban(message, bot) {
 	if (message.content.startsWith("h$ban")) {
-		// Easy way to get member object though mentions.
-		var member = message.mentions.members.first();
-		if (message.mentionned)
-		// Ban
-		member.ban().then((member) => {
-			// Successmessage
-			message.channel.send({embed: { color: 0xFF0000, description:":wave: " + member.displayName + " a été ban du serveur :ballot_box_with_check:"}}).catch(console.erreur);
-		}).catch(() => {
-			// Failmessage
-			message.channel.send({embed: { color: 0xFF0000, description:":x: **Erreur :** Vous n'avez pas les permissions !"}}).catch(console.erreur);
-		});
+		let raison = message.content.substr(28);
+        let banMember = message.guild.member(message.mentions.users.first());
+        if(!message.member.hasPermissions("BAN_MEMBERS")){
+            message.channel.send(`:x: ${message.author} Tu n'as pas la permission de ban les membres.`)
+            return;
+        }else{
+            if(!banMember){
+                message.channel.send(`:x: ${message.author} veuillez mentionner un utilisateur à ban.`)
+                return;
+
+          }else{
+              if(!banMember.bannable){
+                  message.channel.send(`:x: ${message.author} je peux pas kick cet utilisateur veuillez vérifier mes rôles et permissions.`)
+                  return;
+            }else{
+                if(banMember.hasPermission("ADMINISTRATOR")){
+                    message.channel.send(`:x: ${message.author} cet utilisateur est un admin, je peux pas faire ça.`)
+                    return; 
+                }else{
+        message.delete(message.author);
+        message.guild.member(banMember).ban({reason: `${raison}`});
+        message.channel.sendMessage(`${banMember} a été ban! raison:${raison}`);
+                }
+            }
+          }
+	   }
 	}
 }
 
--- modules/modo/kick.js
+++ modules/modo/kick.js
@@ -1,16 +1,30 @@
 function kick(message, bot) {
 	if (message.content.startsWith("h$kick")) {
-		// Easy way to get member object though mentions.
-		var member = message.mentions.members.first();
-		// Kick
-		member.kick().then((member) => {
-			// Successmessage
-			message.channel.send({embed: { color: 0xFF0000, description:":wave: " + member.displayName + " à bien été kické :point_right:"}});
-		}).catch(() => {
-			// Failmessage
-			message.channel.send({embed: { color: 0xFF0000, description:"Accès refusé"}});
-		});
+		let raison = message.content.substr(28);
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!message.member.hasPermissions("KICK_MEMBERS")){
+            message.channel.send(`:x: ${message.author}, Tu n'as pas la permission de kick les membres.`)
+            return;
+        }else{
+            if(!kickMember){
+                message.channel.send(`:x: ${message.author}, veuillez mentionner un utilisateur à kick.`)
+                return;
+          }else{
+              if(!kickMember.kickable){
+                  message.channel.send(`:x: ${message.author}, je peux pas kick cet utilisateur veuillez vérifier mes rôles et permissions.`)
+                  return;
+            }else{
+                if(kickMember.hasPermission("ADMINISTRATOR")){
+                    message.channel.send(`:x: ${message.author}, cet utilisateur est un admin, je peux pas faire ça.`)
+                    return; 
+                }else{
+        message.delete(message.author);
+        message.guild.member(kickMember).kick({reason: `${raison}`});
+        message.channel.sendMessage(`${kickMember} a été kick! raison:${raison}`);
+                	}
+            	}
+        	}
+    	}
 	}
 }
-
 module.exports = kick;
