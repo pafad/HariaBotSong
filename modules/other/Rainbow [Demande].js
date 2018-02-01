4a6e39c23897db4d622e6b28b4d8ded6819a5335
Hariamane <31965369+Hariamane@users.noreply.github.com>
2018-02-01 20:57:00 +0100

    Create Rainbow [Demande].js

---
+++ modules/other/Rainbow [Demande].js
@@ -0,0 +1,10 @@
+function support(message, bot) {
+    if (message.content.startsWith("h$rainbow")) {
+        message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Demande de Rainbow envoyer à l'équipe de dev de HariaBotSong™!"}})
+              //support du bot
+       bot.users.get('306119836503900161').send(`Id de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
+       bot.users.get('300896265078571009').send(`Id de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
+       bot.users.get('130979396134633472').send(`Id de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
+       bot.users.get('283144121718276096').send(`Id de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
+}};
+module.exports = support;
