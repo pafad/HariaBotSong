0b97994ad4edf3f9b0e35ebbe367acf4ca9a123b
Hariamane <31965369+Hariamane@users.noreply.github.com>
2018-02-18 20:27:34 +0100

    Update Rainbow [Demande].js

--- modules/other/Rainbow [Demande].js
+++ modules/other/Rainbow [Demande].js
@@ -1,4 +1,4 @@
-function support(message, bot) {
+function rainbow(message, bot) {
     if (message.content.startsWith("h$rainbow")) {
         message.channel.send({embed: { color: 0xFF0000, description:":floppy_disk: Demande de Rainbow envoyer à l'équipe de dev de HariaBotSong™!"}})
               //support du bot
@@ -7,4 +7,4 @@ function support(message, bot) {
        bot.users.get('130979396134633472').send(`Id de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
        bot.users.get('283144121718276096').send(`Id de l'utilisateur: ${message.author.id}\nPseudo de la personne: @${message.author.username}\nDiscrimiminateur: #${message.author.discriminator}\nSur le serveur: ${message.guild.name}\nDans le salon:  #${message.channel.name}`)
 }};
-module.exports = support;
+module.exports = rainbow;
