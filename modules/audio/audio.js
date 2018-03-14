e66194e92a83a5d0b4367773bcb9937d0abf6d5c
Hariamane <31965369+Hariamane@users.noreply.github.com>
2018-03-14 14:53:48 +0100

    Update audio.js

--- modules/audio/audio.js
+++ modules/audio/audio.js
@@ -1,9 +1,8 @@
 function audio(bot) {
 	const fs = require('fs');
-       const ytdl = require('ytdl-core');
-	ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
-        .pipe(fs.createWriteStream('video.flv'));
         const yt = require("ytdl-core");
+	yt('http://www.youtube.com/watch?v=A02s8omM_hI')
+        .pipe(fs.createWriteStream('video.flv'));
 	var search = require("youtube-search");
 	var request = require("request");
 	var opts = {
