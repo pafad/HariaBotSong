506c3d2b0a2ca4ddba1bf7bc2b07fb3ed8ffb11f
Kévin <kevinfleury.todae@gmail.com>
2018-06-02 16:57:48 +0200

    Update file.

--- modules/modo/purge.js
+++ modules/modo/purge.js
@@ -1,16 +1,35 @@
-function purge(message, bot){
-if(message.content === "h$purge"){
-if(!message.member.hasPermission("MANAGE_MESSAGES")){
-    message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'as pas les permission de gérer les messages`)
-    return;
-}else{
-let messagecount = parseInt(args[0]) ? parseInt(args[0]) : 1;
-message.channel.fetchMessages({limit: 100})
-.then(messages => {
-let msg_array = messages.array();
-msg_array.length = messagecount + 1;
-msg_array.map(m => m.delete().catch(console.error));
-})};
+function purge(message, bot) {
+    if(message.content === "h$purge") {
+        let myrole = message.guild.member(client.user).hasPermission("MANAGE_MESSAGES"); //Récupère les droits nécessaires
+		let yourole = message.guild.member(message.author).hasPermission("MANAGE_MESSAGES"); //Récupère les droits nécessaires
+
+		if (!myrole) { 
+			return message.author.send("<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Je n'ai pas les permissions nécessaires pour effacer un/des message(s)");
+		}
+
+		if (!yourole) {
+			return message.author.send("<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Vous n'avez pas les permissions nécessaires");
+		}
+
+		const user = message.mentions.users.first();
+		const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2]);
+
+		if (!amount) return message.reply("vous devez spécifier un nombre de messages à supprimer !");
+		if (!amount && !user) return message.reply("vous devez spécifier un utilisateur et le nombre, ou juste une quantité, de messages à supprimer !");
+
+		message.channel.fetchMessages({
+			limit: amount,
+		}).then((messages) => {
+			if (user) {
+				const filterBy = user ? user.id : client.user.id;
+				messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
+			}
+			message.channel.bulkDelete(messages).then(ok =>
+				message.channel.send("Suppression de " + amount + " message(s)")
+				.then(message => setTimeout(function(){message.delete()}, 1000))
+			).catch(error => console.log(error.stack));
+		});
     }
 }
-module.exports = purge
+
+module.exports = purge;
