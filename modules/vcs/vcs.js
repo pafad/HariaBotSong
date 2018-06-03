e0454673abf0a3ac23fdfd7d6b4cbced7328bdc1
Kévin <kevinfleury.todae@gmail.com>
2018-06-03 15:30:43 +0200

    Update vcs.js

--- modules/vcs/vcs.js
+++ modules/vcs/vcs.js
@@ -1,22 +1,22 @@
 function vcs(message, bot) {
     if(message.content === "h$vcs") {
         if(message.channel.type === "dm") return;
-            if(!message.guild.channels.exists("name", "vcs")) {
-                message.channel.send("je n'ai pas trouvé de channel nommé `vcs` fait h$vcsadd pour en créer un.")
+            if(!message.guild.channels.exists("name", "haria-vcs")) {
+                message.channel.send("je n'ai pas trouvé de channel nommé `haria-vcs` fait h$vcsadd pour en créer un.")
                 return;
             } else {
-                if(message.channel.name !== "vcs") {
+                if(message.channel.name !== "haria-vcs") {
                     return;
                 } else {
-                    bot.channels.findAll("name", "vcs").map(c => c.send({
+                    bot.channels.findAll("name", "haria-vcs").map(c => c.send({
                         embed: {
                             color: Math.floor(Math.random() * 16777214) + 1,
                             thumbnail: { 
                                 url: message.author.avatarURL 
                             }, 
                             fields: [{
-                                name: `${message.author.tag}(${message.author.id})`, 
-                                value: message.content.substr(5)
+                                name: `${message.author.tag} (${message.author.id})`, 
+                                value: message.content.substr(7)
                             },
                             { 
                                 name: ":gear: -> Serveur:",
