e5a290cdc1ae667a90d91197b28b2f5dc3730e70
Kévin <kevinfleury.todae@gmail.com>
2018-06-03 18:00:52 +0200

    Update vcs.js

--- modules/vcs/vcs.js
+++ modules/vcs/vcs.js
@@ -1,37 +1,31 @@
 function vcs(message, bot) {
     if(message.content === "h$vcs") {
-        var args = message.content.split(' ')[1];
-        
         if(message.channel.type === "dm") return;
         
         if(!message.guild.channels.exists("name", "haria-vcs")) {
             message.channel.send("je n'ai pas trouvé de channel nommé `haria-vcs` fait h$vcsadd pour en créer un.");
             return;
         } else {
-            if(message.channel.name !== "haria-vcs") {
-                return;
-            } else {
-                bot.channels.findAll("name", "haria-vcs").map(c => c.send({
-                    embed: {
-                        color: Math.floor(Math.random() * 16777214) + 1,
-                        thumbnail: { 
-                            url: message.author.avatarURL 
-                        }, 
-                        fields: [{
-                            name: `${message.author.tag} (${message.author.id})`, 
-                            value: args
-                        },
-                        { 
-                            name: ":gear: -> Serveur :",
-                            value: message.guild.name 
-                        }],
-                        timestamp: new Date(), 
-                        footer: { 
-                            text: "vcs"
-                        } 
+            bot.channels.findAll("name", "haria-vcs").map(c => c.send({
+                embed: {
+                    color: Math.floor(Math.random() * 16777214) + 1,
+                    thumbnail: { 
+                        url: message.author.avatarURL 
+                    }, 
+                    fields: [{
+                        name: `${message.author.tag} (${message.author.id})`, 
+                        value: message.content.split(" ")
+                    },
+                    { 
+                        name: ":gear: -> Serveur :",
+                        value: message.guild.name 
+                    }],
+                    timestamp: new Date(), 
+                    footer: { 
+                        text: "vcs"
                     } 
-                }));
-            }
+                } 
+            }));
         }
     }
 }
