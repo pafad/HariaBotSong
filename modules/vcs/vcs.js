94ae89e0e35dc5d0762c11c82a81f85503a4bab8
Kévin <kevinfleury.todae@gmail.com>
2018-06-03 16:08:33 +0200

    Update vcs.js

--- modules/vcs/vcs.js
+++ modules/vcs/vcs.js
@@ -5,13 +5,14 @@ function vcs(message, bot) {
         if(message.channel.type === "dm") return;
         
         if(!message.guild.channels.exists("name", "haria-vcs")) {
-            message.channel.send("je n'ai pas trouvé de channel nommé `haria-vcs` fait h$vcsadd pour en créer un.")
+            message.channel.send("je n'ai pas trouvé de channel nommé `haria-vcs` fait h$vcsadd pour en créer un.");
             return;
         } else {
             if(message.channel.name !== "haria-vcs") {
                 return;
             } else {
-                bot.channels.findAll("name", "haria-vcs").map(c => c.send({
+                /* bot.channels.findAll("name", "haria-vcs").map(c => c.send({ */
+                message.channels.findAll("name", "haria-vcs").map(c => c.send({
                     embed: {
                         color: Math.floor(Math.random() * 16777214) + 1,
                         thumbnail: { 
@@ -30,7 +31,7 @@ function vcs(message, bot) {
                             text: "vcs"
                         } 
                     } 
-                }))
+                }));
             }
         }
     }
