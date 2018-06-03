7d9d283d1fbfa860a2ddd93086f9153690c5aae2
Kévin <kevinfleury.todae@gmail.com>
2018-06-03 17:57:45 +0200

    Update vcs_ban.js

--- modules/vcs/vcs_ban.js
+++ modules/vcs/vcs_ban.js
@@ -3,44 +3,42 @@ function vcs_ban(message, bot) {
         const fs = require("fs");
         const superagent = require("superagent");
         
-        function vcs_ban(bot, message) {
-            var args = message.content.split(" ");
-            
-            if(message.author.id === "300896265078571009") {
-                var url = "https://api.myjson.com/bins/gguwi";
-                
-                request(url, (err, res, body) => {    
-                    console.log('Chargement ...');
-                    
-                    if(err || res.statusCode !== 200) return;
-                    console.log('Chargé avec succès !');
-                    
-                    var objet = JSON.parse(body);
-                    objet[args] = {};
+        var args = message.content.split(" ");
 
-                    request({ url: url, method: 'PUT', json: objet});
-                    bot.channels.findAll('name', 'haria-vcs').map(c => c.send('', {
-                        embed: {
-                            color: Math.floor(Math.random() * 16777214) + 1,
-                            fields: [{
-                                name:':gear: -> Bannissement du vcs',
-                                value:`${message.author.tag} a banni: ${args} du vcs`
-                            },
-                            {
-                                name: `Depuis le serveur : ${message.guild.name}`,
-                                value: ":gear: -> Raison : " + args[1]
-                            }],
-                            timestamp: new Date(),
-                            footer: {
-                                text: `vcs-ban`,
-                            }
+        if(message.author.id === "300896265078571009") {
+            var url = "https://api.myjson.com/bins/gguwi";
+
+            request(url, (err, res, body) => {    
+                console.log('Chargement ...');
+
+                if(err || res.statusCode !== 200) return;
+                console.log('Chargé avec succès !');
+
+                var objet = JSON.parse(body);
+                objet[args] = {};
+
+                request({ url: url, method: 'PUT', json: objet});
+                bot.channels.findAll('name', 'haria-vcs').map(c => c.send('', {
+                    embed: {
+                        color: Math.floor(Math.random() * 16777214) + 1,
+                        fields: [{
+                            name:':gear: -> Bannissement du vcs',
+                            value:`${message.author.tag} a banni: ${args} du vcs`
+                        },
+                        {
+                            name: `Depuis le serveur : ${message.guild.name}`,
+                            value: ":gear: -> Raison : " + args[1]
+                        }],
+                        timestamp: new Date(),
+                        footer: {
+                            text: `vcs-ban`,
                         }
-                    }));
-                })
-            } else {
-                message.channel.send("tu n'est pas un modérateur du vcs")
-                return;
-            }
+                    }
+                }));
+            })
+        } else {
+            message.channel.send("tu n'est pas un modérateur du vcs")
+            return;
         }
     }
 }
