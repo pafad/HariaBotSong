a2358c555eaf98f9b58f96ca26ff82758fcc99d1
Hariamane <31965369+Hariamane@users.noreply.github.com>
2018-06-03 10:59:39 +0200

    Update help.js

--- modules/help.js
+++ modules/help.js
@@ -3,21 +3,21 @@ function help (message, bot, prefix){
 		message.channel.send({embed:{ color: 0xFF0000, description:":envelope_with_arrow: Help envoyer en mp !"}})
 	     message.author.send({embed:{footer:{text: "By !✨⌖αɓςϯɾαɕƘ-δαɾƘ⌖✨",icon_url: "https://cdn.discordapp.com/avatars/300896265078571009/599f13a95e220e4e24e16480d6635648.png?size=2048"},color: 0xFF0000, 
 					 description: 
-					 "**Commandes utils :**\n\n" +
+					 "**Commandes utils :**\n" +
 					":white_check_mark:h$annonce : Permet de faire un annonce dans son propre serveur.\n" +
 					":white_check_mark:h$afk : si vous êtes indisponible.\n"+
 					":white_check_mark:h$botmessage : Cette commande est réservé au développeur.\n" +
 					":white_check_mark:h$join : Cette commande te permet d'ajouter ce bot dans ton serveur.\n" +
 					":white_check_mark:h$discord : Cette commande te permet de me rejoindre.\n" +
 					":white_check_mark:h$support : Cette commande te permet de contacter le support technique du bot.\n\n" +
-					"**Autres commandes :**\n\n"+
+					"**Autres commandes :**\n"+
 					":x:h$mp : Envoyer un message privé a quelqu'un.\n" +
 					":white_check_mark:h$Rainbow : Demandez un rainbow pour son serveur.\n" +
 					":white_check_mark:h$say : Faire parler le bot.\n" +
 					":white_check_mark:h$botconnected : Pour savoir si le bot est connecté.\n" +
 					":white_check_mark:h$ping : Renvoie pong avec le temps écoulé en ms (millisecondes).\n" +
                                         ":white_check_mark:h$vcs : Chat virtuelle pour comuniqué entre serveur sans pouvoir si rendre.\n\n" +
-					"**Commandes modérateur :**\n\n" +
+					"**Commandes modérateur :**\n" +
 					":white_check_mark:h$ban : Cette commande c'est pour ban un utilisateur.\n" +
 					":white_check_mark:h$kick : Cette commande c'est pour kicker un utilisateur.\n" +
 					":x:h$mute : Cette commande c'est pour mute les gens en texte.\n" +
@@ -25,7 +25,7 @@ function help (message, bot, prefix){
 					"**Commandes radio :**\n\n" +
 					":white_check_mark:h$webradioplay <flux_radio> : Pour écouter de la radio.\n" +
 					":white_check_mark:h$webradiostop : Pour stopper la radio.\n\n" +
-					"**Commandes audio :**\n\n" +
+					"**Commandes audio :**\n" +
 					":white_check_mark:h$helpaudio: Listes des commandes audio (MP).\n" +
 					":white_check_mark:h$helpaudioici: Listes des commandes audio (serveur)."
 			}
