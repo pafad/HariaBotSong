7cf208b053e9439ab83e619ad975ba2fb0200238
Kévin <kevinfleury.todae@gmail.com>
2018-06-03 14:48:21 +0200

    Update vcs_ban.js

--- modules/vcs/vcs_ban.js
+++ modules/vcs/vcs_ban.js
@@ -1,41 +1,48 @@
-const fs = require("fs")
-const superagent = require("superagent")
-function vcs_ban(bot, message) {
-var args = message.content.split(" ")
-if(message.author.id == "300896265078571009"){
-var url = "https://api.myjson.com/bins/gguwi";
-request(url, (err, res, body) => {
-    
-    console.log('Chargement...')
-    
-    if(err || res.statusCode!== 200)return;
-    
-    console.log('chargé avec succès !')
-    var objet = JSON.parse(body)
-objet[args] = {};
+function vcs_ban(message, bot) {
+    if(message.content === "h$vcsban") {
+        const fs = require("fs");
+        const superagent = require("superagent");
+        
+        function vcs_ban(bot, message) {
+            var args = message.content.split(" ");
+            
+            if(message.author.id == "300896265078571009") {
+                var url = "https://api.myjson.com/bins/gguwi";
+                
+                request(url, (err, res, body) => {    
+                    console.log('Chargement ...');
+                    
+                    if(err || res.statusCode!== 200) return;
+                    console.log('Chargé avec succès !');
+                    
+                    var objet = JSON.parse(body);
+                    objet[args] = {};
 
-request({ url: url, method: 'PUT', json: objet})
-client.channels.findAll('name', 'vcs').map(c => c.send('',{
- embed: {
-color: Math.floor(Math.random() * 16777214) + 1,
-fields:[{
-    name:':gear: -> Bannissement du vcs',
-    value:`${message.author.tag} a banni: ${args} du vcs`
-    },
-    {
-    name: `Depuis le serveur: ${message.guild.name}`,
-    value: ":gear: -> Raison:" + args[1]
-}],
-timestamp: new Date(),
-footer: {
-text: `vcs-ban`,
-}
-}
-            }));
-        })
-    }else{
-    message.channel.send("tu n'est pas un modérateur du vcs")
-    return;
+                    request({ url: url, method: 'PUT', json: objet});
+                    client.channels.findAll('name', 'vcs').map(c => c.send('', {
+                        embed: {
+                            color: Math.floor(Math.random() * 16777214) + 1,
+                            fields: [{
+                                name:':gear: -> Bannissement du vcs',
+                                value:`${message.author.tag} a banni: ${args} du vcs`
+                            },
+                            {
+                                name: `Depuis le serveur : ${message.guild.name}`,
+                                value: ":gear: -> Raison:" + args[1]
+                            }],
+                            timestamp: new Date(),
+                            footer: {
+                                text: `vcs-ban`,
+                            }
+                        }
+                    }));
+                })
+            } else {
+                message.channel.send("tu n'est pas un modérateur du vcs")
+                return;
+            }
+        }
     }
 }
+
 module.exports = vcs_ban;
