9f8d1feb67408f2a2b3365a5f5efa40dc6bf9f59
Kévin <kevinfleury.todae@gmail.com>
2018-06-06 19:19:17 +0200

    Delete botmessage.js

--- modules/owner/botmessage.js
+++
@@ -1,8 +0,0 @@
-function botmessage(message, bot) {
-  if(message.content.startsWith('h$botmessage')&&message.author.id=='300896265078571009') {
-      serv = bot.guilds.array()
-      text = message.content.substr(12)
-      for(i=0;i<serv.length;i++){
-      serv[i].channels.find('type','text').send(text)
-}}};
-module.exports = botmessage;
