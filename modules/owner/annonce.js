428770efca0c1c871880462e5ae801295b401acf
Kévin <kevinfleury.todae@gmail.com>
2018-06-06 19:22:24 +0200

    Delete annonce.js

--- modules/owner/annonce.js
+++
@@ -1,7 +0,0 @@
-function annonce(message, bot) {
-	if(message.content.startsWith("h$annonce")) {
-               var messageactu = message.content.substr(9);
-			message.channel.send("Hey @everyone, " + messageactu);
-			message.delete(messageactu)}};
-
-module.exports = annonce;
