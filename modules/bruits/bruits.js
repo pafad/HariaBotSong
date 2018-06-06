62cdcb9a62228da0a486edd9dad7ac5f30333613
Kévin <kevinfleury.todae@gmail.com>
2018-06-06 19:22:53 +0200

    Delete bruits.js

--- modules/bruits/bruits.js
+++
@@ -1,5 +0,0 @@
-function bruits(message, bot) {
-	if (message.content.startsWith("h$bruits")) {
-  message.channel.send({embed: { color: 0xFF0000, description:"**Listes des commandes  bruits:**\n\n:x:Les commandes son en dev!:x:"}});
-  }};
-  module.exports = bruits;
