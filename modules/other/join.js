a03e9a142f3328bc19123c23b1f0991dd4ef0402
Kévin <kevinfleury.todae@gmail.com>
2018-06-06 19:45:33 +0200

    Delete join.js

--- modules/other/join.js
+++
@@ -1,6 +0,0 @@
-function join(message, bot) {
-	if (message.content === "h$join") {
-		message.channel.send("https://discordapp.com/oauth2/authorize?client_id=329551988222066689&scope=bot&permissions=-1");
-	}
-}
-module.exports = join;
