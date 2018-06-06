e254321cb8425abd3c582320832140c167b81697
Kévin <kevinfleury.todae@gmail.com>
2018-06-06 19:17:50 +0200

    Delete say.js

--- modules/owner/say.js
+++
@@ -1,7 +0,0 @@
-function say (message, bot) {
-    if (message.content.startsWith("h$say")) {
-        var msg = message.content.substr(6);
-        message.delete(message.author)
-        message.channel.send(msg);
-    }};
-module.exports = say;
