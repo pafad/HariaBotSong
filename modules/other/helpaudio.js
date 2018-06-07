c4ddf4b5017b23881be8492f510ed890bcd75a38
Kévin <kevinfleury.todae@gmail.com>
2018-06-07 10:35:54 +0200

    Update helpaudio.js

--- modules/other/helpaudio.js
+++ modules/other/helpaudio.js
@@ -1,5 +1,5 @@
 function helpaudio(message, prefix, client) {
-	if (message.content.startsWith(prefix + "helpaudio")) {
+	if (message.content.startsWith(prefix + "audiohelp")) {
 		message.channel.send({
 			embed: {
 				color: 0xFF0000,
@@ -15,19 +15,19 @@ function helpaudio(message, prefix, client) {
 				},
 				color: 0xFF0000,
 				description: "**[Commandes audio :]**\n\n" +
-				":white_check_mark:"+ prefix +"playyt <recherche> ou "+ prefix +"playyt <URL vidéo YouTube> ou "+ prefix +"playyt <URL playlist YouTube> : Pour ajouter une musique recherché ou l'URL de la vidéo ou la playlist, met le bot dans un canal vocal et met en lecture la musique.\n" +
-				":white_check_mark:"+ prefix +"skip : Pour arrêter la musique en cours de lecture et de passer à la suivante.\n" +
-				":white_check_mark:"+ prefix +"stop : Pour arrêter la musique en cours, efface la file d'attente (s'il y en a) et quitte le canal vocal.\n" +
-				":white_check_mark:"+ prefix +"volume ou "+ prefix +"volume <0, 5> : Affiche le volume actuel ou régler le volume entre 0 et 5.\n" +
-				":white_check_mark:"+ prefix +"np : Affiche la musique en cours de lecture.\n" +
-				":white_check_mark:"+ prefix +"queue :  Pour afficher la/les musique(s) qui est/sont dans la file d'attente.\n" +
-				":white_check_mark:"+ prefix +"pause : Pour mettre la musique en cours de lecture en pause.\n" +
-				":white_check_mark:"+ prefix +"resume : Pour remettre la musique qui était en pause de nouveau en lecture."
+				":white_check_mark: "+ prefix +"playyt <recherche> ou "+ prefix +"playyt <URL vidéo YouTube> ou "+ prefix +"playyt <URL playlist YouTube> : Pour ajouter une musique recherché ou l'URL de la vidéo ou la playlist, met le bot dans un canal vocal et met en lecture la musique.\n" +
+				":white_check_mark: "+ prefix +"skip : Pour arrêter la musique en cours de lecture et de passer à la suivante.\n" +
+				":white_check_mark: "+ prefix +"stop : Pour arrêter la musique en cours, efface la file d'attente (s'il y en a) et quitte le canal vocal.\n" +
+				":white_check_mark: "+ prefix +"volume ou "+ prefix +"volume <0, 5> : Affiche le volume actuel ou régler le volume entre 0 et 5.\n" +
+				":white_check_mark: "+ prefix +"np : Affiche la musique en cours de lecture.\n" +
+				":white_check_mark: "+ prefix +"queue :  Pour afficher la/les musique(s) qui est/sont dans la file d'attente.\n" +
+				":white_check_mark: "+ prefix +"pause : Pour mettre la musique en cours de lecture en pause.\n" +
+				":white_check_mark: "+ prefix +"resume : Pour remettre la musique qui était en pause de nouveau en lecture."
 			}
 		});
 	}
 
-	if (message.content.startsWith(prefix + "helpaudioici")) {
+	if (message.content.startsWith(prefix + "audioherehelp")) {
 		message.channel.send({
 			embed: {
 				footer: {
@@ -36,14 +36,14 @@ function helpaudio(message, prefix, client) {
 				},
 				color: 0xFF0000,
 				description: "**[Commandes audio :]**\n\n" +
-				":white_check_mark:"+ prefix +"playyt <recherche> ou "+ prefix +"playyt <URL vidéo YouTube> ou "+ prefix +"playyt <URL playlist YouTube> : Pour ajouter une musique recherché ou l'URL de la vidéo ou la playlist, met le bot dans un canal vocal et met en lecture la musique.\n" +
-				":white_check_mark:"+ prefix +"skip : Pour arrêter la musique en cours de lecture et de passer à la suivante.\n" +
-				":white_check_mark:"+ prefix +"stop : Pour arrêter la musique en cours, efface la file d'attente (s'il y en a) et quitte le canal vocal.\n" +
-				":white_check_mark:"+ prefix +"volume ou "+ prefix +"volume <0, 5> : Affiche le volume actuel ou régler le volume entre 0 et 5.\n" +
-				":white_check_mark:"+ prefix +"np : Affiche la musique en cours de lecture.\n" +
-				":white_check_mark:"+ prefix +"queue :  Pour afficher la/les musique(s) qui est/sont dans la file d'attente.\n" +
-				":white_check_mark:"+ prefix +"pause : Pour mettre la musique en cours de lecture en pause.\n" +
-				":white_check_mark:"+ prefix +"resume : Pour remettre la musique qui était en pause de nouveau en lecture."
+				":white_check_mark: "+ prefix +"playyt <recherche> ou "+ prefix +"playyt <URL vidéo YouTube> ou "+ prefix +"playyt <URL playlist YouTube> : Pour ajouter une musique recherché ou l'URL de la vidéo ou la playlist, met le bot dans un canal vocal et met en lecture la musique.\n" +
+				":white_check_mark: "+ prefix +"skip : Pour arrêter la musique en cours de lecture et de passer à la suivante.\n" +
+				":white_check_mark: "+ prefix +"stop : Pour arrêter la musique en cours, efface la file d'attente (s'il y en a) et quitte le canal vocal.\n" +
+				":white_check_mark: "+ prefix +"volume ou "+ prefix +"volume <0, 5> : Affiche le volume actuel ou régler le volume entre 0 et 5.\n" +
+				":white_check_mark: "+ prefix +"np : Affiche la musique en cours de lecture.\n" +
+				":white_check_mark: "+ prefix +"queue :  Pour afficher la/les musique(s) qui est/sont dans la file d'attente.\n" +
+				":white_check_mark: "+ prefix +"pause : Pour mettre la musique en cours de lecture en pause.\n" +
+				":white_check_mark: "+ prefix +"resume : Pour remettre la musique qui était en pause de nouveau en lecture."
 			}
 		});
 	}
