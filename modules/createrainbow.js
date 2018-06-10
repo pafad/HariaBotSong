f545e0bc34f991540804ab2ec99145c993322d2d
Kévin <kevinfleury.todae@gmail.com>
2018-06-10 10:47:50 +0200

    Update createrainbow.js

--- modules/createrainbow.js
+++ modules/createrainbow.js
@@ -1,9 +1,9 @@
-exports.run = (client, message, args) => {
-     if(message.author.id !== "306119836503900161"){
-    message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'es pas mon developpeur.`)
-    return;
-}else{
-message.guild.createRole({name: "↻"})
-message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role rainbow créé avec succès !`)
+function (client, message, args) => {
+     if(message.author.id !== "306119836503900161") {
+          message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'es pas mon developpeur.`);
+          return;
+     } else {
+          message.guild.createRole({name: "↻"})
+          message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role rainbow créé avec succès !`)
     }
 }
